65359871a9e402ea189eb1ca9be7ae441aac24a1
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-09-24 22:58:54 +0700

    create layout

---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import { Outlet } from "react-router-dom";
+
+const MainLayout = () => {
+  return (
+    <main>
+      <Outlet />
+    </main>
+  );
+};
+
+export default MainLayout;
