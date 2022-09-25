43ea6ed4b62a05c26f4475a2d315809b666e85ad
chon26909 <59905927+chon26909@users.noreply.github.com>
2022-09-25 22:11:31 +0700

    create router

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,34 @@ import { FC } from "react";
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
-import AdminPage from "./pages/AdminPage";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import RequireAuth from "./components/RequireAuth";
+import ManageAccount from "./pages/ManageAccount";
+import AuthProvider from "./context/AuthProvider";
+import PageNotFound from "./pages/PageNotFound";
+
+const ROLES = {
+  user: 1,
+  admin: 2,
+  supervisor: 10,
+};
 
 const App: FC = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route path="admin" element={<AdminPage />} />
-        </Route>
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="/" element={<MainLayout />}>
+            <Route element={<RequireAuth allowedRoles={[ROLES.supervisor]} />}>
+              <Route index element={<DashboardPage />} />
+              <Route path="uma" element={<ManageAccount />} />
+            </Route>
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 };
